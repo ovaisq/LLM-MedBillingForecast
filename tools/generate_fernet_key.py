b69157a89189cba74ae6760d8ed6a98e3aef8168
ovaisq <ovaisq@icloud.com>
2024-03-04 12:56:42 -0800

    updated key gen script

--- tools/generate_fernet_key.py
+++ tools/generate_fernet_key.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """This module provides a function for generating and saving an
-	encryption key to a file using the Fernet cryptography library.
+    encryption key to a file using the Fernet cryptography library.
 
    ©2024, Ovais Quraishi
 """
@@ -9,19 +9,20 @@ from pathlib import Path
 from cryptography.fernet import Fernet
 
 def generate_and_save_key(filename):
-	"""Generates and saves an encryption key to a file using the Fernet cryptography library.
-	
-		Args:
-			filename (str or pathlib.Path): The name of the file to save the key to.
+    """Generates and saves an encryption key to a file using the Fernet cryptography library.
+    
+        Args:
+            filename (str or pathlib.Path): The name of the file to save the key to.
 
-		Returns:
-			None
-	"""
+        Returns:
+            None
+    """
 
     if not filename.exists():
         key = Fernet.generate_key()
         with open(filename, 'wb') as key_file:
             key_file.write(key)
+        print(f'Encrption Key File {filename} created')
     else:
         print(f'{filename} already exists!')
 
